d6b703b41356f6af1248a650748ad39675137e39
joaovictordejesus <166874133+joaovictordejesus@users.noreply.github.com>
2024-06-14 14:20:46 -0300

    Update aula1_questão4.py

---
+++ Módulo 5/aula1_questão4.py
@@ -0,0 +1,10 @@
+# Mostra a data e horário atuais e formata
+import datetime
+
+data_hora_atual = datetime.datetime.now()
+
+data_formatada = data_hora_atual.strftime("Data: %d/%m/%Y")
+hora_formatada = data_hora_atual.strftime("Hora: %H:%M")
+
+print(data_formatada)
+print(hora_formatada)
