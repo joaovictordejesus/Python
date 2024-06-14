b89ce5cf538b8c0f59064ac524d600856e50221f
joaovictordejesus <166874133+joaovictordejesus@users.noreply.github.com>
2024-06-14 13:43:10 -0300

    Update aula4_questão2.py

--- Módulo 6/aula4_questão2.py
+++ Módulo 6/aula4_questão2.py
@@ -1,10 +1,9 @@
-# Solicitar a frase do usuário
+# Solicitar a frase do usuário depois mostra Lista de vogais e consoantes e após isso Imprimir as listas
+
 frase = input("Digite uma frase: ")
 
-# Lista de vogais e consoantes
 vogais = sorted([char for char in frase if char.lower() in 'aeiou'])
 consoantes = [char for char in frase if char.lower() in 'bcdfghjklmnpqrstvwxyz']
 
-# Imprimir as listas
 print("Vogais:", vogais)
 print("Consoantes:", consoantes)
