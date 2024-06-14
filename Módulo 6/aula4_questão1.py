2951ba6111d509635c927d3f24f5950b1f9dae2f
joaovictordejesus <166874133+joaovictordejesus@users.noreply.github.com>
2024-06-14 13:42:21 -0300

    Update aula4_questão1.py

--- Módulo 6/aula4_questão1.py
+++ Módulo 6/aula4_questão1.py
@@ -1,7 +1,14 @@
-pares_20_50 = [x for x in range(20, 51) if x % 2 == 0]
+# x definido como 0 para inciar o código, depois, o código define os números pares entre 20 e 50, os quadrados de 1 a 9, os divisíveis por 7 de 1 a 100 e paridade
 
-quadrados = [x**2 for x in range(1, 10)]
+x=0 
 
-divisiveis_por_7 = [x for x in range(1, 101) if x % 7 == 0]
+pares_20_50 = [i for i in range(20, 51) if i % 2 == 0]
+print("Pares entre 20 e 50:", pares_20_50)
 
-paridade = ['par' if x % 2 == 0 else 'ímpar' for x in range(0, 30, 3)]
+quadrados = [i**2 for i in range(1, 10)]
+print("Quadrados de 1 a 9:", quadrados)
+
+divisiveis_por_7 = [i for i in range(1, 101) if i % 7 == 0]
+print("Divisíveis por 7 entre 1 e 100:", divisiveis_por_7)
+6
+paridade = ['par' if i % 2 == 0 else 'ímpar' for i in range(0, x, 3)]
