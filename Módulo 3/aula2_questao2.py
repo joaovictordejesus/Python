d7dc7fd9067d8e27749de314b0c586bfa56f754e
joaovictordejesus <166874133+joaovictordejesus@users.noreply.github.com>
2024-06-14 13:11:25 -0300

    Update aula2_questao2.py

--- Módulo 3/aula2_questao2.py
+++ Módulo 3/aula2_questao2.py
@@ -1,6 +1,8 @@
 # Verificar se pelo menos uma é maior de 17
+
 idade_juliana = int(input("Digite a idade de Juliana: "))
 idade_cris = int(input("Digite a idade de Cris: "))
 
 podem_entrar = idade_juliana > 17 or idade_cris > 17
+
 print(podem_entrar)
